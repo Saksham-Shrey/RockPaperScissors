6fe0043e3b7f1f263c44fc13f4a2daedfa90d7fd
Saksham Shrey <sakshamshrey_work@icloud.com>
2024-02-14 12:34:34 +0530

    added the project files to upstream

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Player1: UIImageView!
+    @IBOutlet weak var Player2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        Player1.image = UIImage(imageLiteralResourceName: "InitialBackground")
+        
+        Player2.image = UIImage(imageLiteralResourceName: "InitialBackground")
+        
     }
-
-
+    
+    let rockpaperscissorsInverted = [UIImage(imageLiteralResourceName: "RockInverted"),
+                            UIImage(imageLiteralResourceName: "PaperInverted"),
+                            UIImage(imageLiteralResourceName: "ScissorsInverted")]
+    
+    let rockpaperscissors = [UIImage(imageLiteralResourceName: "Rock"),
+                            UIImage(imageLiteralResourceName: "Paper"),
+                            UIImage(imageLiteralResourceName: "Scissors")]
+    
+    @IBAction func goButtonPressed(_ sender: Any) {
+        Player1.image = rockpaperscissorsInverted.randomElement()
+        
+        Player2.image = rockpaperscissors.randomElement()
+        
+    }
+    
 }
 
